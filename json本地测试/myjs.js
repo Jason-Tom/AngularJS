41114136f54c3eeaf1a2f6db033cd9f043c80081
Jason-Tom <429295712@qq.com>
2016-04-19 21:48:01 +0800

    里面嵌套单个变量的和数组也行 只要对应变量对应数组就行

--- json本地测试/myjs.js
+++ json本地测试/myjs.js
@@ -16,25 +16,28 @@
     //     episode: "18"
     // }]
 
-    var JSONObject = [{
-        source: "zimuzu",
-        title: "十二猴子 (12 Monkeys) S2E1",
-        source_id: "33231",
-        cnname: "十二猴子",
-        enname: "12 Monkeys",
-        season: "2",
-        episode: "1"
-    }, {
-        source: "zimuzu",
-        title: "疯狂前女友 (Crazy Ex-Girlfriend) S1E18",
-        source_id: "33806",
-        cnname: "疯狂前女友",
-        enname: "Crazy Ex-Girlfriend",
-        season: "1",
-        episode: "18"
-    }]
+    var JSONObject = {
+        code: "sdfs",
+        shuzu:[{
+            source: "zimuzu",
+            title: "十二猴子 (12 Monkeys) S2E1",
+            source_id: "33231",
+            cnname: "十二猴子",
+            enname: "12 Monkeys",
+            season: "2",
+            episode: "1"
+        }, {
+            source: "zimuzu",
+            title: "疯狂前女友 (Crazy Ex-Girlfriend) S1E18",
+            source_id: "33806",
+            cnname: "疯狂前女友",
+            enname: "Crazy Ex-Girlfriend",
+            season: "1",
+            episode: "18"
+        }]
+    }
     var app = angular.module('myApp', []);
     app.controller('myCtrl', function($scope) {
-        $scope.data = JSONObject;
+        $scope.data = JSONObject.shuzu;
     });
     // document.getElementById("jname").innerHTML = JSONObject.data.result[0].title
